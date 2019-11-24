55afe841f966e332cbd76dec1153f39d20e1f47b
mxwllndrsn <mxwllndrsn@gmail.com>
2019-11-24 00:12:05 -0800

    snake object draw success

--- snake.js
+++ snake.js
@@ -10,37 +10,58 @@ canvas.width = document.body.clientWidth;
 var x = canvas.width/2;
 var y = canvas.height/2;
 
-ctx.fillStyle='black';
-ctx.fillRect(x, y, 10, 10);
 
-function keyDown(e){
-    if(e.key=='w'){
-        clearCanvas();
-        y-=10;
-        ctx.fillRect(x, y, 10, 10);
-        console.log('s');
-    }
-    if(e.key=='s'){
-        clearCanvas();
-        y+=10;
-        ctx.fillRect(x, y, 10, 10);
-        console.log('w');
-    }
-    if(e.key=='a'){
-        clearCanvas();
-        x-=10;
-        ctx.fillRect(x, y, 10, 10);
-        console.log('a');
-    }
-    if(e.key=='d'){
-        clearCanvas();
-        x+=10;
-        ctx.fillRect(x, y, 10, 10);
-        console.log('d');
-    }
-}
-
-function clearCanvas(){
+function clear(){
     canvas.width = canvas.width;
 }
 
+function snake(){
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
+    this.w = 10;
+    this.h = 10;
+    this.segments = 1;
+
+    this.draw = function(){
+        clear();
+        ctx.fillStyle='black';
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        console.log('drawing');
+    };
+    this.setX = function(x){
+        this.x += x;
+        this.draw();
+    };
+    this.setY = function(y){
+        this.y += y;
+        this.draw();
+    };
+    this.getX = function(){
+        return this.x;
+    };
+    this.getY = function(){
+        return this.y;
+    }
+};
+
+var s = new snake();
+s.draw();
+
+function keyDown(e){
+    if(e.key=='w'){
+        s.setY(-10);
+        console.log('w');
+    }
+    if(e.key=='s'){
+        s.setY(10);
+        console.log('s');
+    }
+    if(e.key=='a'){
+        s.setX(-10);
+        console.log('a');
+    }
+    if(e.key=='d'){
+        s.setX(10);
+        console.log('d');
+    }
+}
